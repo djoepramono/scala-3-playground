2e2710ee8982553a897d406f094a8c2619be4597
Djoe Pramono <djoepramono@gmail.com>
2023-02-28 23:47:51 +1100

    create helper file for monadTransformer module

--- src/main/scala/playground/monadTransformer/beginning.scala
+++ src/main/scala/playground/monadTransformer/beginning.scala
@@ -4,27 +4,8 @@ import scala.concurrent.Future
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
-implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
-
-case class User(name: String)
-
-def getUser(name: String): Future[Option[User]] =
-  if name == "Joe"
-  then Future(Some(User("Joe")))
-  else Future(None)
-
-def getScores(user: User): Future[Option[Int]] =
-  if user.name == "Joe"
-  then Future(Some(100))
-  else Future(None)
-
-def giveConstructiveFeedback(score: Int): String = {
-  if score > 50
-  then "good job Joe"
-  else "not bad Joe"
-}
-
 object Beginning {
+
   def main(args: Array[String]): Unit = {
 
     val x = for {
