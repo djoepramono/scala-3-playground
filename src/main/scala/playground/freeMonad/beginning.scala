7bf3994fa9e22e89cb8df2e753ec10980a7ce1a5
Djoe Pramono <djoepramono@gmail.com>
2023-04-25 13:08:04 +1000

    rejig code location for freeMonad beginning

--- src/main/scala/playground/freeMonad/beginning.scala
+++ src/main/scala/playground/freeMonad/beginning.scala
@@ -12,75 +12,75 @@ import scala.concurrent.duration.Duration
 import cats.effect.unsafe.implicits._
 import cats.effect.IO
 
-case class User(name: String)
+object Beginning {
 
-// ActionA = Action Algebra
-// The algebra contains case classes
-sealed trait ActionA[A]
-case class GetUser(name: String) extends ActionA[Option[User]]
-case class GetScore(user: User) extends ActionA[Option[Int]]
+  case class User(name: String)
 
-// Action[A] represents a free (gratis) monad for your algebra
-type Action[A] = Free[ActionA, A]
+  // ActionA = Action Algebra
+  // The algebra contains case classes
+  sealed trait ActionA[A]
+  case class GetUser(name: String) extends ActionA[Option[User]]
+  case class GetScore(user: User) extends ActionA[Option[Int]]
 
-// Too many type parameters to look for, several type parameters can be removed
-// - liftF type parameter can be omitted
-// - The algebra type parameter can be omitted
-// getUser is a helper function albeit like higher order function
-//   that turn the algebra into a ready to use function that is returns a monad
+  // Action[A] represents a free (gratis) monad for your algebra
+  type Action[A] = Free[ActionA, A]
 
-def getUser(name: String): Action[Option[User]] =
+  // Too many type parameters to look for, several type parameters can be removed
+  // - liftF type parameter can be omitted
+  // - The algebra type parameter can be omitted
+  // getUser is a helper function albeit like higher order function
+  //   that turn the algebra into a ready to use function that returns a monad
+  def getUser(name: String): Action[Option[User]] =
     liftF[ActionA, Option[User]](GetUser(name))
 
-def getScore(user: User): Action[Option[Int]] =
+  def getScore(user: User): Action[Option[Int]] =
     liftF(GetScore(user))
 
-implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
-
-// The intepreter would translate GetUser[A](name: String) to Future[Option[A]]
-// The interpreter result would need to be monadic
-// Future is a monad and not a monad at the same time.
-//   This is because Future evaluates eagerly
-//   Future(100) is a monad, Future(Random.nextInt(100)) is not a monad
-//     because it's not referentially transparent
-val futureInterpreter = new (ActionA ~> Future) {
+  // The intepreter would translate GetUser[A](name: String) to Future[Option[A]]
+  // The interpreter result would need to be monadic
+  // Future is a monad and not a monad at the same time.
+  //   This is because Future evaluates eagerly
+  //   Future(100) is a monad, Future(Random.nextInt(100)) is not a monad
+  //     because it's not referentially transparent
+  implicit val ec: scala.concurrent.ExecutionContext =
+    scala.concurrent.ExecutionContext.global
+  val futureInterpreter = new (ActionA ~> Future) {
     override def apply[A](fa: ActionA[A]): Future[A] = fa match {
-        case GetUser(name) => Future.successful(Some(User("Always Joe")))
-        case GetScore(user) => Future.successful(Some(100))
+      case GetUser(name)  => Future.successful(Some(User("Always Joe")))
+      case GetScore(user) => Future.successful(Some(100))
     }
-}
+  }
 
-// We can also use different interpreters e.g. use IO instead of Future
-val ioInterpreter = new (ActionA ~> IO) {
+  // We can also use different interpreters e.g. use IO instead of Future
+  val ioInterpreter = new (ActionA ~> IO) {
     override def apply[A](fa: ActionA[A]): IO[A] = fa match {
-        case GetUser(name) => IO(Some(User("Always Joe")))
-        case GetScore(user) => IO(Some(100))
+      case GetUser(name)  => IO(Some(User("Always Joe")))
+      case GetScore(user) => IO(Some(100))
     }
-}
+  }
 
-object FreeMonad {
-    def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {
 
-        // we can use Monad Transformer from the previous session
-        // we need the type parameter, otherwise it'll be treated as Any
-        // make sense because it's Action[Option[A]], which mean it can be anything
-        val maybeScore = for {
-            joe <- OptionT(getUser("Joe"))
-            score <- OptionT(getScore(joe))
-        } yield score
+    // we can use Monad Transformer from the previous session
+    // we need the type parameter, otherwise it'll be treated as Any
+    // make sense because it's Action[Option[A]], which mean it can be anything
+    val maybeScore = for {
+      joe <- OptionT(getUser("Joe"))
+      score <- OptionT(getScore(joe))
+    } yield score
 
-        // Remember since maybeScore is a Monad Transformer
-        // We need to extract the value (Free Monad)
-        // foldMap is basically running the Free Monads i.e. Action[A] and accumulate the result into a monad
+    // Remember since maybeScore is a Monad Transformer
+    // We need to extract the value (Free Monad)
+    // foldMap is basically running the Free Monads i.e. Action[A] and accumulate the result into a monad
 
-        // With futureInterpreter
-        // val x = maybeScore.value.foldMap(futureInterpreter)
-        // println(Await.result(x, Duration(100, "millis")))
+    // With futureInterpreter
+    // val x = maybeScore.value.foldMap(futureInterpreter)
+    // println(Await.result(x, Duration(100, "millis")))
 
-        // With IOInterpreter
-        val x = maybeScore.value.foldMap(ioInterpreter)
-        println(x.unsafeRunSync())
-    }
+    // With IOInterpreter
+    val x = maybeScore.value.foldMap(ioInterpreter)
+    println(x.unsafeRunSync())
+  }
 }
 
 // Questions
