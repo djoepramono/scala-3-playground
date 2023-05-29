282912adedd6623f98638f6f553225b848e06d6a
Djoe Pramono <djoepramono@gmail.com>
2023-05-29 18:14:42 +1000

    add Tagless Finaln example

---
+++ src/main/scala/playground/taglessFinal/beginning.scala
@@ -0,0 +1,36 @@
+package playground.taglessFinal
+
+import cats.Monad
+import cats.effect.IO
+import cats.implicits._
+import cats.effect.unsafe.implicits._
+
+object Beginning {
+
+  case class User(name: String)
+
+  // Unlike Free, the algebra is a trait with some methods describing the repository
+  trait ActionRepository[F[_]] {
+    def getUser(name: String): F[User]
+    def getScore(user: User): F[Int]
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    class Program[F[_]: Monad](ar: ActionRepository[F]) {
+      def run(name: String): F[Int] = for {
+        user <- ar.getUser(name)
+        score <- ar.getScore(user)
+      } yield score
+    }
+
+    trait ActionInterpreter extends ActionRepository[IO] {
+      override def getUser(name: String) = IO(User("Joe"))
+      override def getScore(user: User): IO[Int] = IO(10)
+    }
+
+    val result: IO[Int] = new Program(new ActionInterpreter {}).run("Joe")
+    println(result.unsafeRunSync())
+  }
+
+}
