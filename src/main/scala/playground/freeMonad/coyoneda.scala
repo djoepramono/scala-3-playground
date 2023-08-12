0f589b79a2fd0084d33afbdea20235565d54a886
Djoe Pramono <djoepramono@gmail.com>
2023-08-12 18:33:15 +1000

    add more instructions on coyoneda

--- src/main/scala/playground/freeMonad/coyoneda.scala
+++ src/main/scala/playground/freeMonad/coyoneda.scala
@@ -4,44 +4,55 @@ import cats.Functor
 
 object Coyoneda {
 
-  // Given a type constructor F[A], get me a Functor for it
+  // Given a type constructor F[A], get me a Functor for it so that I can modify the inner value without changing the container
+  // F[A] to F[B] without F.map
 
   // High level steps
-  // - underlyingValue and transformation as context
-  // - implement FreeFunctor.map with liftF
-
-  // map is running A => B, lifting B to F[B]: Monad, and flatMap on F[B]: Monad
-  // A => B => C
+  // - we need to lift F[A] into another container type that has a map and run,
+  //   - `lift` to put everything in a new container. The new container type needs to know both F and A, let's say C[F[_],A]
+  //   - `map` to run the transformation
+  //   - `run` to turn the container back to F[B]
+  // - Other than the methods, we would need to know the context too
+  //   - underlyingValue, F[A]
+  //   - transformation, the function that would change A to B
+  // - Tricks
+  //   - apart from underlyingValue we would also need UnderlyingType
+  //   - start with `lift`
+  //   - continue with `run`, turning F[UnderlyingType] to F[A]
+  //   - implement `map` with `lift`
+  //   - implement `run` with the help of functor parameter
 
   trait FreeFunctor[F[_], A] {
-    type UnderlyingType // it's the type inside the type constructor
-    val underlyingValue: F[
-      UnderlyingType
-    ] // it's the type constructor that we'd like to turn into a Functor, which is F[A]
+    // underlyingValue also contains underlyingType
+    type UnderlyingType
+    val underlyingValue: F[UnderlyingType]
     val transformation: UnderlyingType => A
-    def map[B](f: A => B): FreeFunctor[F, B] =
-      FreeFunctor.liftF(underlyingValue)(transformation andThen f)
 
-    // Executing Functor's map
-    // Turning FreeFunctor[A] to F[A]
+    // Turn underlyingValue or F[UnderlyingType] => F[A]
     def run(f: Functor[F]): F[A] = f.map(underlyingValue)(transformation)
+
+    // Implement `map` with the helpf of `lift`
+    def map[B](f: A => B): FreeFunctor[F, B] =
+      FreeFunctor.lift(underlyingValue)(transformation andThen f)
   }
 
   // Given F[A] and A => B, give me a FreeFunctor[F, B]
   object FreeFunctor {
-    def liftF[F[_], A, B](fa: F[A])(f: A => B): FreeFunctor[F, B] = new FreeFunctor[F, B] {
+    def lift[F[_], A, B](fa: F[A])(f: A => B): FreeFunctor[F, B] = new FreeFunctor[F, B] {
       type UnderlyingType = A
       val underlyingValue: F[A] = fa
       val transformation: A => B = f
     }
   }
 
+  // Let's see things in action
   // Imagine there's a type constructor to Action
   case class Action[A](input: A)
 
   def main(args: Array[String]): Unit = {
     // Turn Action[A] into Functor
-    val coyoneda = FreeFunctor.liftF(Action(2))(_ + 2)
+    // the second parameter can be an identity function e.g. +0 or *1
+    val coyoneda = FreeFunctor.lift(Action(2))(_ + 2)
     val coyoneda2 = coyoneda.map(_ + 3)
 
     val actionFunctor = new Functor[Action] {
